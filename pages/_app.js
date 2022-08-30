6aa5009e03ba8a9c505121fbe16510397527e642
anhlha <luuhoaianh.tb@gmail.com>
2022-08-31 01:48:28 +0700

    Add redux-saga

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,21 @@ import React from 'react'
 import '../assets/globals.css'
 import Header from "../components/header.component"
 import Footer from "../components/footer.component"
+import createSagaMiddleware from 'redux-saga'
+import {createStore, applyMiddleware} from 'redux'
+import rootReducer from '../reducers/index'
+import rootSaga from '../sagas/index'
+import { createWrapper } from 'next-redux-wrapper';
 
+export const makeStore = (context)=>{
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+
+  return store;
+}
+
+const wrapper = createWrapper(makeStore, { debug: true })
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -15,4 +29,4 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
