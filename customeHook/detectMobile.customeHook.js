c5dc4da7c89d030347d42ea186786f078cda0dc6
tung <hivetech.dev01@gmail.com>
2022-09-18 21:14:20 +0700

    Add title

--- customeHook/detectMobile.customeHook.js
+++ customeHook/detectMobile.customeHook.js
@@ -4,7 +4,8 @@ import { commonConst } from "../constants";
 
 const useDetectMobile = (style) => {
     useEffect(() => {
-        const isMobile = navigator.userAgentData.mobile;
+        //const isMobile = navigator.userAgentData.mobile;
+        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
         if(isMobile){
             window.location.href = commonConst.REDIRECT_URL
         }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../assets/globals.css'
 import { Header, Footer, IconFixed } from "../components/index"
 import createSagaMiddleware from 'redux-saga'
@@ -24,16 +24,20 @@ function MyApp({ Component, pageProps }) {
 
   useDetectMobile()
 
+  useEffect(() => {
+    window.title = '33333'
+  }, [])
+
   return (
     <>
       <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="keywords" content="Super,chinese, ứng, dụng, hoc, tiếng, trung, chính, hãng, số 1, việt, nam" />
         <meta name="description" content="Super chinese ứng dụng học tiếng Trung chính hãng số 1 Việt Nam, ứng dụng học tiếng Trung giá rẻ tốt nhất 2022, ứng dụng học tiếng trung HSK mới nhất " />
         <meta name="generator" content="Cty TNHH Konec" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="" />
-        <link rel="icon" href="/images/favicon.ico" type="image/png" />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
         <script async="" src="//za.zdn.vn/v3/za.js?19251"></script><script async="" src="https://www.googletagmanager.com/gtag/js?id=G-2RF48MKB7N"></script>
         {/* <script>
             window.dataLayer = window.dataLayer || [];
@@ -42,13 +46,13 @@ function MyApp({ Component, pageProps }) {
 
             gtag('config', 'G-2RF48MKB7N');
           </script> */}
+        <meta property="og:title" content="Tiếng Trung siêu việt" key="title" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&amp;display=swap"
           rel="stylesheet"
         />
-        <title>Tiếng Trung siêu việt</title>
       </Head>
 
       <Header />
